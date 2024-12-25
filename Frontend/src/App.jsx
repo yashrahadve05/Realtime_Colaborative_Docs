d39d1f7d5bfce3f17337b001c61fd88e645ce823
yashrahadve <yashrahadve05@gmail.com>
2024-12-25 15:52:09 +0530

    Update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './components/Home'
-// import Login from './components/Login'
-// import Signup from './components/Signup'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import "./App.css";
+import Login from './components/Login'
+import Register from './components/register'
+import Navbar from './components/Navbar'
+import LandingPage from "./components/LandingPage";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home />
-      {/* <Login /> */}
-      {/* <Signup /> */}
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
